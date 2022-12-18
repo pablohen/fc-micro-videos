0b3d4dfd6c123512e8ff8cb08d4d56a7ddd5abf7
Pablo Henrique <pablo_hen@hotmail.com>
2022-12-18 13:26:37 -0300

    feat: add more tests

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,10 +1,156 @@
-import { Category } from "./category";
+import { omit } from "lodash";
+import { v4 as uuidV4, validate as uuidValidate } from "uuid";
+import { Category, Props } from "./category";
 
-describe("Category Tests", () => {
-  test("should have correct name", () => {
-    const categoryName = "Movie";
-    const category = new Category(categoryName);
+interface CategoryData {
+  props: Props;
+  id?: string;
+}
 
-    expect(category.name).toBe(categoryName);
+describe("Category Unit Tests", () => {
+  test("should have valid Category data", () => {
+    let category = new Category({ name: "Movie" });
+
+    let props = omit(category.props, "created_at");
+
+    expect(props).toStrictEqual({
+      name: "Movie",
+      description: null,
+      is_active: true,
+    });
+
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+    });
+
+    let created_at = new Date();
+
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+      created_at,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "other description",
+    });
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    created_at = new Date();
+
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at,
+    });
+  });
+
+  test("should have valid id", () => {
+    const data: CategoryData[] = [
+      {
+        props: { name: "Movie" },
+      },
+      {
+        props: { name: "Movie" },
+        id: null,
+      },
+      {
+        props: { name: "Movie" },
+        id: undefined,
+      },
+      {
+        props: { name: "Movie" },
+        id: uuidV4(),
+      },
+    ];
+
+    data.forEach((item) => {
+      let category = new Category(item.props, item.id);
+      expect(category.id).not.toBeNull();
+      expect(uuidValidate(category.id)).toBeTruthy();
+    });
+  });
+  test("should have valid name", () => {
+    const category = new Category({ name: "Movie" });
+
+    expect(category.name).toBe("Movie");
+  });
+  test("should have valid description", () => {
+    let category = new Category({
+      name: "Movie",
+    });
+    expect(category.description).toBe(null);
+
+    category = new Category({
+      name: "Movie",
+      description: "description",
+    });
+    expect(category.description).toBe("description");
+
+    category = new Category({
+      name: "Movie",
+    });
+
+    category["description"] = "other description";
+    expect(category.description).toBe("other description");
+
+    category["description"] = undefined;
+    expect(category.description).toBeNull();
+  });
+  test("should have valid is_active", () => {
+    let category = new Category({
+      name: "Movie",
+    });
+    expect(category.is_active).toBeTruthy();
+
+    category = new Category({
+      name: "Movie",
+      is_active: false,
+    });
+    expect(category.is_active).toBe(false);
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.is_active).toBe(true);
+  });
+
+  test("should have valid created_at", () => {
+    let category = new Category({
+      name: "Movie",
+    });
+    expect(category.created_at).toBeInstanceOf(Date);
+
+    let created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+    expect(category.created_at).toBe(created_at);
   });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,43 @@
+import { v4 as uuidV4 } from "uuid";
+
+export interface Props {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+}
+
 export class Category {
-  constructor(public name: string) {}
+  public readonly id: string;
+
+  constructor(public readonly props: Props, id?: string) {
+    this.id = id || uuidV4();
+    this.props.description = this.props.description ?? null;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  private set description(value: string) {
+    this.props.description = value ?? null;
+  }
+
+  get is_active() {
+    return this.props.is_active;
+  }
+
+  private set is_active(value: boolean) {
+    this.props.is_active = value ?? true;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
 }
