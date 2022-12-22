335b97b07522a16d0d54a9a71417217b3f454b6f
Pablo Henrique de Souza <pablo.henrique@cubos.io>
2022-12-22 18:54:36 +0000

    feat(CreateCategoryUseCase): implementation and unit tests

---
+++ src/category/application/use-cases/__tests__/create-category.use-case.spec.ts
@@ -0,0 +1,42 @@
+import { CategoryInMemoryRepository } from "../../../../category/infra/repository/category-in-memory.repository";
+import { CreateCategoryUseCase } from "../create-category.use-case";
+
+describe("CreateCategoryUseCase Unit Tests", () => {
+  let repository: CategoryInMemoryRepository;
+  let useCase: CreateCategoryUseCase;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  test("should create a new category", async () => {
+    const spyInsert = jest.spyOn(repository, "insert");
+
+    let output = await useCase.execute({
+      name: "categoria",
+    });
+    expect(spyInsert).toBeCalledTimes(1);
+    expect(output).toStrictEqual({
+      id: repository.items[0].id,
+      name: repository.items[0].name,
+      description: repository.items[0].description,
+      is_active: repository.items[0].is_active,
+      created_at: repository.items[0].created_at,
+    });
+
+    output = await useCase.execute({
+      name: "categoria 2",
+      description: "desc",
+      is_active: false,
+    });
+    expect(spyInsert).toBeCalledTimes(2);
+    expect(output).toStrictEqual({
+      id: repository.items[1].id,
+      name: repository.items[1].name,
+      description: repository.items[1].description,
+      is_active: repository.items[1].is_active,
+      created_at: repository.items[1].created_at,
+    });
+  });
+});
---
+++ src/category/application/use-cases/create-category.use-case.ts
@@ -0,0 +1,33 @@
+import { Category } from "../../../category/domain/entities/category";
+import { CategoryRepository } from "../../../category/domain/repository/category.repository";
+
+export interface Input {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+}
+
+export interface Output {
+  id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+}
+
+export class CreateCategoryUseCase {
+  constructor(private categoryRepository: CategoryRepository.Repository) {}
+
+  async execute(input: Input): Promise<Output> {
+    const entity = new Category(input);
+    await this.categoryRepository.insert(entity);
+
+    return {
+      id: entity.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at,
+    };
+  }
+}
