36f065a1fda3507e00f5dfc460dacda2cb3411c8
Pablo Henrique <pablo_hen@hotmail.com>
2022-12-28 12:59:00 +0000

    feat: search method tests

--- src/@core/src/category/infra/db/sequelize/category-repository.spec.ts
+++ src/@core/src/category/infra/db/sequelize/category-repository.spec.ts
@@ -1,12 +1,20 @@
-import { Category } from "#category/domain";
+import { Category, CategoryRepository } from "#category/domain";
 import { NotFoundError, UniqueEntityId } from "#seedwork/domain";
 import { setupSequelize } from "#seedwork/infra/testing/helpers/db";
+import _chance from "chance";
+import { CategoryModelMapper } from "./category-mapper";
 import { CategoryModel } from "./category-model";
 import { CategorySequelizeRepository } from "./category-repository";
 
 describe("CategoryRepository Tests", () => {
   setupSequelize({ models: [CategoryModel] });
+  let chance: Chance.Chance;
+
   let repository: CategorySequelizeRepository;
+
+  beforeAll(() => {
+    chance = _chance();
+  });
 
   beforeEach(async () => {
     repository = new CategorySequelizeRepository(CategoryModel);
@@ -74,8 +82,76 @@ describe("CategoryRepository Tests", () => {
     expect(JSON.stringify(entities)).toBe(JSON.stringify([entity]));
   });
 
-  test("should first", async () => {
-    await CategoryModel.factory().create();
-    console.log(await CategoryModel.findAll());
+  describe("search method tests", () => {
+    test("should only apply paginate when other params are null", async () => {
+      const createdAt = new Date();
+
+      await CategoryModel.factory()
+        .count(16)
+        .bulkCreate(() => ({
+          id: chance.guid({ version: 4 }),
+          name: "name",
+          description: null,
+          is_active: true,
+          created_at: createdAt,
+        }));
+
+      const spyToEntity = jest.spyOn(CategoryModelMapper, "toEntity");
+
+      const searchOutput = await repository.search(
+        new CategoryRepository.SearchParams()
+      );
+
+      expect(searchOutput).toBeInstanceOf(CategoryRepository.SearchResult);
+      expect(spyToEntity).toHaveBeenCalledTimes(15);
+      expect(searchOutput.toJSON()).toMatchObject({
+        total: 16,
+        current_page: 1,
+        per_page: 15,
+        last_page: 2,
+        sort: null,
+        sort_dir: null,
+        filter: null,
+      });
+
+      searchOutput.items.forEach((item) => {
+        expect(item).toBeInstanceOf(Category);
+        expect(item.id).toBeDefined();
+      });
+
+      const items = searchOutput.items.map((item) => item.toJSON());
+
+      expect(items).toMatchObject(
+        new Array(15).fill({
+          name: "name",
+          description: null,
+          is_active: true,
+          created_at: createdAt,
+        })
+      );
+    });
+
+    test("should order by created_at DESC when search params are null", async () => {
+      const createdAt = new Date();
+
+      await CategoryModel.factory()
+        .count(16)
+        .bulkCreate((index) => ({
+          id: chance.guid({ version: 4 }),
+          name: `Movie-${index}`,
+          description: null,
+          is_active: true,
+          created_at: new Date(createdAt.getTime() + index * 100),
+        }));
+
+      const searchOutput = await repository.search(
+        new CategoryRepository.SearchParams()
+      );
+
+      const items = searchOutput.items;
+      [...items].reverse().forEach((item, index) => {
+        expect(item.name).toBe(`Movie-${index + 1}`);
+      });
+    });
   });
 });
