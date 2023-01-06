7be57f8e8e4fdd1fbabd0f07a4e025c83eebf670
Pablo Henrique de Souza <pablo.henrique@cubos.io>
2023-01-06 14:37:56 +0000

    ci: env changes

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "#### Starting @core configs..."
 # @core
-if [ ! -f "./src/@core/env.testing" ]; then
+if [ ! -f "./src/@core/env.test" ]; then
   cp ./src/@core/.env.test.example ./src/@core/.env.test
 fi
 
+echo "#### Starting nestjs configs..."
 # nestjs
 if [ ! -f "./src/nestjs/envs/.env" ]; then
     cp ./src/nestjs/.env.example ./src/nestjs/.env
@@ -14,8 +16,13 @@ if [ ! -f "./src/nestjs/envs/.env.test" ]; then
     cp ./src/nestjs/.env.test.example ./src/nestjs/.env.test
 fi
 
+if [ ! -f "./src/nestjs/envs/.env.e2e" ]; then
+    cp ./src/nestjs/.env.e2e.example ./src/nestjs/.env.e2e
+fi
+
 npm i
 
+echo "#### Building @core..."
 npm run build -w @fc/micro-videos
 
 tail -f /dev/null
